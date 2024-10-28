3654f0b01d7acc8861f8fa36d612a6291e9ceb61
Sharrine8 <65632872+Sharrine8@users.noreply.github.com>
2024-10-28 09:32:27 -0500

    initial commit

---
+++ controllers/clothingItems.js
@@ -0,0 +1,95 @@
+const ClothingItem = require("../models/clothingItem");
+
+const getClothingItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((err) => {
+      console.error(err);
+      return res
+        .status(500)
+        .send({ message: `${err.name} with the message ${err.message}` });
+    });
+};
+
+const createClothingItem = (req, res) => {
+  const { name, weather, imageUrl } = req.body;
+  ClothingItem.create({
+    name,
+    weather,
+    imageUrl,
+    owner: "671c09510c5089144beb9553",
+  })
+    .then((item) => res.status(201).send({ data: item }))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res.status(400).send({
+          message: `${err.name} with the message ${err.message}`,
+        });
+      }
+      return res
+        .status(500)
+        .send({ message: `${err.name} with the message ${err.message}` });
+    });
+};
+
+const updateClothingItem = (req, res) => {
+  const { itemId } = req.params;
+  const { imageUrl } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } })
+    .orFail(() => {
+      const error = new Error("Item ID not found");
+      error.statusCode = 404;
+      throw error;
+    })
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((err) => {
+      console.error(err);
+      return res
+        .status(500)
+        .send({ message: `${err.name} with the message ${err.message}` });
+    });
+};
+
+const likeClothingItem = (req, res) =>
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  );
+
+const dislikeClothingItem = (req, res) =>
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    {
+      $pull: { likes: req.user.userId },
+    },
+    { new: true }
+  );
+
+const deleteClothingItem = (req, res) => {
+  const { itemId } = req.params;
+  ClothingItem.findByIdAndRemove({ _id: itemId })
+    .orFail(() => {
+      const error = new Error("Item ID not found");
+      error.statusCode = 404;
+      throw error;
+    })
+    .then((item) => res.status(200).send(item))
+    .catch((err) => {
+      console.error(err);
+      return res
+        .status(500)
+        .send({ message: `${err.name} with the message ${err.message}` });
+    });
+};
+
+module.exports = {
+  getClothingItems,
+  createClothingItem,
+  updateClothingItem,
+  likeClothingItem,
+  dislikeClothingItem,
+  deleteClothingItem,
+};
---
+++ routes/clothingItems.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const {
+  getClothingItems,
+  createClothingItem,
+  updateClothingItem,
+  likeClothingItem,
+  dislikeClothingItem,
+  deleteClothingItem,
+} = require("../controllers/clothingItems");
+
+// CRUD -- Create, Read, Update, Delete
+
+router.post("/", createClothingItem);
+
+router.get("/", getClothingItems);
+
+router.put("/:itemId", updateClothingItem);
+router.put("/:itemId/likes", likeClothingItem);
+router.put("/:itemId/likes", dislikeClothingItem);
+
+router.delete("/:itemId", deleteClothingItem);
+
+module.exports = router;
